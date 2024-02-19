4093c1b1d265aefb8e3d9cc2466aadd2f5c95e15
Elena Kovylyaeva <lengayva@yandex.ru>
2024-02-19 20:26:06 +0500

    someData

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -2,19 +2,21 @@ package ru.netology.rest;
 
 import org.junit.jupiter.api.Test;
 
-
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 public class PostmanEchoTest {
     @Test
-    void shouldReturnSendData
-        var text = "Hi";
+    void shouldReturnSendData(){
+        var text = "some data";
         given()
                 .baseUri("https://postman-echo.com")
-                .body(text)
-// Выполняемые действия
+                .body("some data")
+
                 .when()
                 .post("/post")
-// Проверки
+
                 .then()
                 .statusCode(200)
-                .body(data, equalTo(text));
+                .body("data", equalTo(text));
+    }
 }
